8d874a2c3cf51bd7826e2f5f33d27623216a2a4a
fraites <afraites@vt.edu>
2021-02-17 23:50:37 -0500

    need to figure out how to grab stock slider info; return undefined atm

--- yahoo-finance/stock-scanner.js
+++ yahoo-finance/stock-scanner.js
@@ -11,16 +11,19 @@ async function createBrowser() {
   return page;
 }
 
-//this should read S&P, DOW, NASDAQ, and Russell Futures for the day
-async function readFutures(page){
-  await page.reload();
-  let html = await page.evaluate(() => document.body.innerHTML)
-  console.log(html);
+async function scrape(page){
+  await page.reload({waitUntil: 'domcontentloaded'});
+  let html = await page.content();
+  let sp = await page.$('#marketsummary-itm-0');
+  console.log(sp);
 }
+
+//this should read S&P, DOW, NASDAQ, and Russell Futures for the day
+
 
 async function main() {
   let page = await createBrowser();
-  await readFutures(page);
+  await scrape(page);
 }
 
 main();
