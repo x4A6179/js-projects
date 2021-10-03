c1e1808c3c052b4136865ced5cbbfdf479fa6b3e
x4A6179 <59290923+x4A6179@users.noreply.github.com>
2021-10-03 01:01:00 -0400

    refactored QuoteMachine to initialize state; added randomInt function

--- fcc-projects/quote-machine/src/index.js
+++ fcc-projects/quote-machine/src/index.js
@@ -2,55 +2,58 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-
-
-class Quote extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-
-    );
-  }
-}
-
 class QuoteMachine extends React.Component {
   constructor(props){
     super(props);
+    this.state = {
+      quotes: quotes,
+      currQuote: quotes[0].quote,
+      currAuth: quotes[0].author,
+    }
     this.newQuote = this.newQuote.bind(this);
+    this.randomInt = this.randomInt.bind(this);
+  }
+
+
+  randomInt(){
+    const idx = Math.floor(Math.random() * this.state.quotes.length)
+    return idx;
   }
 
   newQuote() {
+    const index = this.randomInt;
     this.setState({
-      text: '',
-      author: '',
+      text: this.state.quotes[index].quote,
+      author: this.state.quotes[index].author,
     });
   }
 
   render() {
-    const boxStyle = {
-      margin: "0 auto"
-    }
-
-    const quotes = {
-      'A':'Hello',
-      'B':'Bye'
-    }
-
-    const idx = Math.floor(Math.random() * quotes.length);
-
     return (
       <div id='quote-box' style={boxStyle}>
-        <p id='text'></p>
-        <p id='author'></p>
-        <button id='new-quote' onClick={this.newQuote)}></button>
+        <p id='text'>{this.state.currQuote}</p>
+        <p id='author'>{this.state.currAuth}</p>
+        <button id='new-quote' onClick={this.newQuote}></button>
         <a id='tweet-quote' href='https://twitter.com/intent/tweet/' />
       </div>
     );
   }
 }
 
+const quotes = [
+  {
+    quote: "testing1",
+    author: "Auth1"
+  },
+  {
+    quote: "testing2",
+    author: "Auth2"
+  },
+  {
+    quote: "testing3",
+    author: "Auth3"
+  }
+];
 
 
 ReactDOM.render(<QuoteMachine /> ,document.getElementById('root'));
