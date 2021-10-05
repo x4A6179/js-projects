c83240ecc15d0c7ba8188d96f10f2b3b69a74ed6
x4A6179 <59290923+x4A6179@users.noreply.github.com>
2021-10-05 01:23:49 -0400

    created base function for quote.js; creates basic view for rendering random quote; need to initialize random state

--- fcc-projects/quote-machine/src/components/Quote.js
+++ fcc-projects/quote-machine/src/components/Quote.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components'
 
 const Quote = () => {
+
+  const [quote, setQuote] = useState(quotes[0]);
+
+function randomQuote() {
+    setQuote(quotes[Math.floor(Math.random() * quotes.length)]);
+  }
+
   return(
     <div id='quote-box'>
-      <p id='text'></p>
-      <p id='author'></p>
-      <button id='new-quote'></button>
+      <p id='text'>{quote.quote}</p>
+      <p id='author'>{quote.author}</p>
+      <button id='new-quote' onClick={() => randomQuote()}>New Quote</button>
       <a id='tweet-quote' href='twitter.com/intent/tweet'>Tweet Quote</a>
     </div>
   )
 }
+
+const quotes = [
+  {author: "Test", quote: "This is a test"},
+  {author: "Test2", quote: "This is another test"},
+  {author: "test3", quote: "Test 3 coming in"},
+  {author: "Test4", quote: "Here lies test 4"}
+];
+
 
 export default Quote;
