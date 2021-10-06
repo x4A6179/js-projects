be5c73ed70755445607d077ee7aff9e3d941d92d
x4A6179 <59290923+x4A6179@users.noreply.github.com>
2021-10-05 23:07:51 -0400

    new quote functionality updated/working as intended; started styling for site

--- fcc-projects/quote-machine/src/App.js
+++ fcc-projects/quote-machine/src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+//import './App.css';
 import Quote from './components/Quote';
 
 function App() {
--- fcc-projects/quote-machine/src/components/Quote.js
+++ fcc-projects/quote-machine/src/components/Quote.js
@@ -1,30 +1,46 @@
 import React, { useState } from 'react';
-import styled from 'styled-components'
+//import styled from 'styled-components';
+import './Quote.css';
 
 const Quote = () => {
 
-  const [quote, setQuote] = useState(quotes[0]);
+  const [quote, setQuote] = useState(quotes[Math.floor(Math.random() * quotes.length)]);
 
-function randomQuote() {
-    setQuote(quotes[Math.floor(Math.random() * quotes.length)]);
+  const randomQuote = () => {
+    return quotes[Math.floor(Math.random() * quotes.length)]
   }
 
-  return(
-    <div id='quote-box'>
-      <p id='text'>{quote.quote}</p>
-      <p id='author'>{quote.author}</p>
-      <button id='new-quote' onClick={() => randomQuote()}>New Quote</button>
-      <a id='tweet-quote' href='twitter.com/intent/tweet'>Tweet Quote</a>
-    </div>
-  )
-}
+  const genRandomQuote = () => {
+    let newRandom = randomQuote();
+    let prevQuote = quote;
+    while (newRandom === prevQuote) {
+      newRandom = randomQuote();
+    }
+    setQuote(newRandom);
+  }
 
-const quotes = [
-  {author: "Test", quote: "This is a test"},
-  {author: "Test2", quote: "This is another test"},
-  {author: "test3", quote: "Test 3 coming in"},
-  {author: "Test4", quote: "Here lies test 4"}
-];
+    return(
+      <div id='quote-box'>
+        <div className='quote-text'>
+          <p id='text'>{quote.quote}</p>
+        </div>
+        <div className='quote-author'>
+          <span id='author'>-{quote.author}</span>
+        </div>
+        <div className='buttons'>
+          <button className='button' id='new-quote' onClick={() => genRandomQuote()}>New Quote</button>
+          <a className='button' id='tweet-quote' target="_blank" href='twitter.com/intent/tweet'>Tweet Quote</a>
+        </div>
+      </div>
+    )
+  }
+
+  const quotes = [
+    {author: "Test", quote: "This is a test"},
+    {author: "Test2", quote: "This is another test"},
+    {author: "test3", quote: "Test 3 coming in"},
+    {author: "Test4", quote: "Here lies test 4"}
+  ];
 
 
 export default Quote;
