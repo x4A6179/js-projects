158cb0745b27f0be6999eba2983ef7f68a585e7c
x4A6179 <59290923+x4A6179@users.noreply.github.com>
2021-10-08 01:28:20 -0400

    started navbar css; added menu button; needs to handle links & routing

--- fcc-projects/quote-machine/src/components/Navbar.js
+++ fcc-projects/quote-machine/src/components/Navbar.js
@@ -1,54 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './fontawesome';
+import './Navbar.css'
 
 const Navbar = () => {
 
   const [nav, setNav] = useState();
   const [menu, setMenu] = useState(false);
 
-  const handleMenu = () => {
-    
-  }
-
-
   return (
-    <div className='top-bar' style={style}>
+    <div className='top-bar'>
       <div>
-        <NavButton href='#home' className='bar-item nav-button'><FontAwesomeIcon icon='home' /> Home</NavButton>
-        <NavButton href='#quotes' className='bar-item nav-button'><i className='nv-quotes'></i> Quotes</NavButton>
-        <MenuButton className='bar-item menu-button' onClick={handleMenu}><FontAwesomeIcon icon='bars' transform='grow-10' /></MenuButton>
+        <a href='#home' className='bar-item nav-button'><FontAwesomeIcon icon='home' /> Home</a>
+        <a href='#quotes' className='bar-item nav-button'><i className='nv-quotes'></i> Quotes</a>
+        <DropdownMenu />
       </div>
     </div>
   )
 }
 
-const NavButton = styled.a`
-  color: black;
-  font-size: 18px;
-  float: left;
-  margin-right: 10px;
-  margin-top: 15px;
-  padding-left: 5px;
-  text-decoration: none;
+const DropdownMenu = () => {
+  const [isActive, setIsActive] = useState(false);
+  const dropdownRef = useRef(null);
+  const onClick = () => setIsActive(!isActive);
 
-  :hover {
-    color: red;
-  }
-`
-const MenuButton = styled.button`
-  color: black;
-  position: relative;
-  float: right;
-  background-color: transparent;
-  border-shadow: none;
-  border: none;
-  margin-top: 15px;
-  margin-right: 25px;
-`
-const style = {
-  minWidth: '99vw'
+  return (
+    <div className='menu-container'>
+      <button className='bar-item menu-button' onClick={onClick}><FontAwesomeIcon icon='bars' transform='grow-5' /></button>
+      <nav ref={dropdownRef} className={`menu ${isActive ? 'active' : 'inactive'}`}>
+        <ul>
+          <li><a href='/connect'>Connect Wallet</a></li>
+          <li><a href='/balance'>Show Balance</a></li>
+          <li><a href='/signup'>Sign Up</a></li>
+        </ul>
+      </nav>
+    </div>);
 }
+
 
 export default Navbar;
