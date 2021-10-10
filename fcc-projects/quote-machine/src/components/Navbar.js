70f55271658f11e7651912e45458c50c85d4e393
x4A6179 <59290923+x4A6179@users.noreply.github.com>
2021-10-09 23:34:13 -0400

    relocated navbar items; removed quote page/added "about the team" to menu; set up inital function for connecting to web3

--- fcc-projects/quote-machine/src/components/Navbar.js
+++ fcc-projects/quote-machine/src/components/Navbar.js
@@ -1,19 +1,16 @@
 import React, { useState, useRef } from 'react';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { InjectedConnector } from '@web3-react/injected-connector';
+import { ethers } from 'ethers';
 import './fontawesome';
 import './Navbar.css'
 
 const Navbar = () => {
 
-  const [nav, setNav] = useState();
-  const [menu, setMenu] = useState(false);
-
   return (
     <div className='top-bar'>
       <div>
-        <a href='#home' className='bar-item nav-button'><FontAwesomeIcon icon='home' /> Home</a>
-        <a href='#quotes' className='bar-item nav-button'><i className='nv-quotes'></i> Quotes</a>
         <DropdownMenu />
       </div>
     </div>
@@ -30,13 +27,21 @@ const DropdownMenu = () => {
       <button className='bar-item menu-button' onClick={onClick}><FontAwesomeIcon icon='bars' transform='grow-5' /></button>
       <nav ref={dropdownRef} className={`menu ${isActive ? 'active' : 'inactive'}`}>
         <ul>
-          <li><a href='/connect'>Connect Wallet</a></li>
-          <li><a href='/balance'>Show Balance</a></li>
-          <li><a href='/signup'>Sign Up</a></li>
+          <li><a href='/'><FontAwesomeIcon icon='home' /> Home</a></li>
+          <li><a href='/connect' onClick={connectWallet}>Connect Wallet</a></li>
+          <li><a href='/aboutus'>About the Team</a></li>
         </ul>
       </nav>
     </div>);
 }
+
+function connectWallet() {
+
+}
+
+const injected = new InjectedConnector ({
+  supportedChainIds: [1, 3, 4, 5, 42]
+})
 
 
 export default Navbar;
